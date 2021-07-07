a64c2db595eff5cb8ef539d264043bcd750fcbda
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-07-07 15:34:16 +0900

    Feature/category (#2)
    * feat: Add health model
    
    * feat: Set timezone and language and STATIC_ROOT
    
    * feat: Add location serializer
    
    * feat: Add location view
    
    * feat: Add location url
    
    * feat: Add Category model
    
    * feat: Add Category serializer
    
    * feat: Add Category view
    
    * feat: Add Category url

--- category/views.py
+++ category/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from category.models import Category
+from category.serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
