c117f388a36026e3db5d2f9cda0f714b955466ea
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-07-07 16:05:57 +0900

    Feature/finess search (#4)
    * feat: Add Category admin
    
    * refactor: Change Fitness field category start with lower case
    
    * feat: Add nested category & location serializer to fitness serializer
    
    * feat: Add __str__ in location model
    
    * feat: Add __str__ in category model
    
    * feat: Add search filter in fitness view
    
    * feat: Add Djangofilter to fitness view

--- fitness/serializers.py
+++ fitness/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from category.serializers import CategorySerializer
+from location.serializers import LocationSerializer
 from .models import Fitness
 
 
 class FitnessSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    location = LocationSerializer()
+
     class Meta:
         model = Fitness
-        fields = ["id"]
+        fields = ["id", "name", "navigation", "location", "category"]
--- fitness/views.py
+++ fitness/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 
 from fitness.models import Fitness
 from fitness.serializers import FitnessSerializer
@@ -7,3 +8,7 @@ from fitness.serializers import FitnessSerializer
 class FitnessViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Fitness.objects.all()
     serializer_class = FitnessSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
+    filterset_fields = ['location_id', 'category_id']
