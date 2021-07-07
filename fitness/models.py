5f399bcee497f795859b10779e7c65db567fc004
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-07-07 16:05:57 +0900

    Feature/finess search (#4)
    * feat: Add Category admin
    
    * refactor: Change Fitness field category start with lower case
    
    * feat: Add nested category & location serializer to fitness serializer
    
    * feat: Add __str__ in location model
    
    * feat: Add __str__ in category model
    
    * feat: Add search filter in fitness view
    
    * feat: Add Djangofilter to fitness view

--- fitness/models.py
+++ fitness/models.py
@@ -5,7 +5,7 @@ class Fitness(models.Model):
     name = models.CharField(max_length=20)
     navigation = models.CharField(max_length=255)
     location = models.ForeignKey("location.Location", on_delete=models.CASCADE)
-    Category = models.ForeignKey("category.Category", on_delete=models.CASCADE)
+    category = models.ForeignKey("category.Category", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- location/models.py
+++ location/models.py
@@ -5,5 +5,8 @@ class Location(models.Model):
     city = models.CharField(max_length=50)
     region = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.city}_{self.region}"
+
     class Meta:
         db_table = "location"
