b931f534ef53e77d50b34dc78807fb116aedd4f6
sjwhole <bluemoon2718@naver.com>
2021-07-07 15:52:37 +0900

    feat: Add search filter in fitness view

--- fitness/views.py
+++ fitness/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from fitness.models import Fitness
 from fitness.serializers import FitnessSerializer
@@ -7,3 +7,6 @@ from fitness.serializers import FitnessSerializer
 class FitnessViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Fitness.objects.all()
     serializer_class = FitnessSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
