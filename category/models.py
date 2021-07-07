3318f78699cd62f64d5b005d65e0b9dea87ef7c0
sjwhole <bluemoon2718@naver.com>
2021-07-07 15:32:44 +0900

    feat: Add Category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "category"
