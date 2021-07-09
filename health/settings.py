98c45307bc7d0bf25a93d4139db05685851d7c0e
sjwhole <bluemoon2718@naver.com>
2021-07-09 16:54:28 +0900

    feat: Add allow CORS

--- health/settings.py
+++ health/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     # 3rd party
     "rest_framework",
     "django_filters",
+    'corsheaders',
     # App
     "fitness",
     "category",
@@ -64,11 +65,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'health.urls'
 
