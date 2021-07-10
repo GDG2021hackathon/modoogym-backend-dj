9b4ee1d91e4f53c937f02c0d1cb328866993a090
sjwhole <bluemoon2718@naver.com>
2021-07-11 00:20:09 +0900

    feat: Add if user not login like false

--- membership/serializers.py
+++ membership/serializers.py
@@ -12,14 +12,16 @@ class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
         fields = ["id", "title", "price", "end_date", "description", "image", "like", "fitness", "fitness_name",
-                  "seller",
-                  "seller_nickname"]
+                  "seller", "seller_nickname"]
         read_only_fields = ["seller"]
 
     def get_image(self, membership):
         return membership.fitness.sub_image
 
     def get_like(self, membership):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
         if membership in self.context.get('request').user.favorites.all():
             return True
         else:
@@ -28,8 +30,18 @@ class MembershipSerializer(serializers.ModelSerializer):
 
 class MyMembershipSerializer(serializers.ModelSerializer):
     fitness_name = serializers.CharField(source="fitness.name", read_only=True)
+    like = serializers.SerializerMethodField()
 
     class Meta:
         model = Membership
-        fields = ["id", "title", "price", "validation", "end_date", "description", "fitness", "fitness_name"]
+        fields = ["id", "title", "price", "validation", "end_date", "like", "description", "fitness", "fitness_name"]
         read_only_fields = ["validation"]
+
+    def get_like(self, membership):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        if membership in self.context.get('request').user.favorites.all():
+            return True
+        else:
+            return False
