0c7e2bf491b39316b9af6a2eb605186104c8f42e
sjwhole <bluemoon2718@naver.com>
2021-07-10 23:35:03 +0900

    feat: Add like to member serializer

--- membership/serializers.py
+++ membership/serializers.py
@@ -7,15 +7,23 @@ class MembershipSerializer(serializers.ModelSerializer):
     fitness_name = serializers.CharField(source="fitness.name", read_only=True)
     seller_nickname = serializers.CharField(source="seller.nickname", read_only=True)
     image = serializers.SerializerMethodField()
+    like = serializers.SerializerMethodField()
 
     class Meta:
         model = Membership
-        fields = ["id", "title", "price", "end_date", "description", "image", "fitness", "fitness_name", "seller",
+        fields = ["id", "title", "price", "end_date", "description", "image", "like", "fitness", "fitness_name",
+                  "seller",
                   "seller_nickname"]
         read_only_fields = ["seller"]
 
     def get_image(self, membership):
         return membership.fitness.sub_image
+
+    def get_like(self, membership):
+        if membership in self.context.get('request').user.favorites.all():
+            return True
+        else:
+            return False
 
 
 class MyMembershipSerializer(serializers.ModelSerializer):
