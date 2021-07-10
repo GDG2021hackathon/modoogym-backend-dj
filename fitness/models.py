2d0e6dc4de4e45aac3532fb37270a3ed70681de8
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-07-10 17:35:46 +0900

    Feature/fitness score count (#17)
    * feat: Add score to fitness model
    
    * feat: Add score & counts to fitness serializer
    
    * feat: Change name counts to count

--- fitness/models.py
+++ fitness/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Fitness(models.Model):
     name = models.CharField(max_length=20)
+    score = models.FloatField()
     image = models.CharField(max_length=255)
     navigation = models.CharField(max_length=255)
     location = models.ForeignKey("location.Location", on_delete=models.CASCADE)
--- fitness/serializers.py
+++ fitness/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 
 from category.serializers import CategorySerializer
 from location.serializers import LocationSerializer
+from membership.models import Membership
 from .models import Fitness
 
 
 class FitnessSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     location = LocationSerializer()
+    count = serializers.SerializerMethodField(default=0)
 
     class Meta:
         model = Fitness
-        fields = ["id", "name", "image", "navigation", "location", "category"]
+        fields = ["id", "name", "score", "count", "image", "navigation", "location", "category"]
+
+    def get_count(self, fitness):
+        return len(Membership.objects.filter(fitness=fitness, validation=True))
