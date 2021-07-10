5ac6802102efb44809837aea9db0c336f0fb4929
sjwhole <bluemoon2718@naver.com>
2021-07-10 16:49:53 +0900

    feat: Add cash to user model

--- user/serializers.py
+++ user/serializers.py
@@ -6,4 +6,5 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "nickname"]
+        fields = ["id", "email", "nickname", "cash"]
+        read_only_fields = ["cash"]
