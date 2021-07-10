3355223b37113a6cc5e326941b8a7ef02116099b
sjwhole <bluemoon2718@naver.com>
2021-07-10 17:33:01 +0900

    feat: Add score & counts to fitness serializer

--- fitness/serializers.py
+++ fitness/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 
 from category.serializers import CategorySerializer
 from location.serializers import LocationSerializer
+from membership.models import Membership
 from .models import Fitness
 
 
 class FitnessSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     location = LocationSerializer()
+    counts = serializers.SerializerMethodField(default=0)
 
     class Meta:
         model = Fitness
-        fields = ["id", "name", "image", "navigation", "location", "category"]
+        fields = ["id", "name", "score", "counts", "image", "navigation", "location", "category"]
+
+    def get_counts(self, fitness):
+        return len(Membership.objects.filter(fitness=fitness, validation=True))
