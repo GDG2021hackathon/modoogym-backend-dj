b6328a820ff1049f12c58fc2238f9a21a8f7537d
sjwhole <bluemoon2718@naver.com>
2021-07-10 23:46:54 +0900

    feat: Add list function to membership view

--- membership/views.py
+++ membership/views.py
@@ -11,13 +11,24 @@ from .serializers import MembershipSerializer, MyMembershipSerializer
 
 
 class MembershipViewSet(viewsets.ModelViewSet):
-    queryset = Membership.objects.filter(validation=True)
+    queryset = Membership.objects.all()
     serializer_class = MembershipSerializer
 
     permission_classes = [IsOwnerOrReadOnly]
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['fitness_id']
+
+    def list(self, request, *args, **kwargs):
+        queryset = Membership.objects.filter(validation=True)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
