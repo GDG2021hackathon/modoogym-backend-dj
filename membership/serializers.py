ba887a0094a0dbc13992751169b758da6e9005eb
sjwhole <bluemoon2718@naver.com>
2021-07-10 17:04:14 +0900

    feat: Add seller nickname to MembershipSerializer

--- membership/serializers.py
+++ membership/serializers.py
@@ -5,10 +5,11 @@ from .models import Membership
 
 class MembershipSerializer(serializers.ModelSerializer):
     fitness_name = serializers.CharField(source="fitness.name", read_only=True)
+    seller_nickname = serializers.CharField(source="seller.nickname", read_only=True)
 
     class Meta:
         model = Membership
-        fields = ["id", "price", "end_date", "description", "fitness", "fitness_name", "seller"]
+        fields = ["id", "price", "end_date", "description", "fitness", "fitness_name", "seller", "seller_nickname"]
         read_only_fields = ["seller"]
 
 
